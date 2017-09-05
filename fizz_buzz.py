ba4aec1f8a8c977a0a97dbed8a726d9c29b626a6
Evan Harris <echarris@smcm.edu>
2017-09-04 23:11:17 -0400

    Accepts user input as limit

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,13 +1,19 @@
-index = 0
+import sys
 
-while index != 16:
-    if index % 15 == 0:
+counter = 1
+
+if len(sys.argv) == 1:
+    index = int(input('Pick any positive number: '))
+else:
+    index = int(sys.argv[1])
+
+while counter <= index:
+    if counter % 15 == 0:
         print('Fizz buzz counting up to fizzbuzz')
-    elif index % 5 == 0:
+    elif counter % 5 == 0:
         print('Fizz buzz counting up to buzz')
-    elif index % 3 == 0:
+    elif counter % 3 == 0:
         print('Fizz buzz counting up to fizz')
     else:
-        print('Fizz buzz counting up to {}'.format(index))
-    index += 1
-
+        print('Fizz buzz counting up to {}'.format(counter))
+    counter += 1
