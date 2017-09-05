9adfff2f7769364c82cad87ac3c6ef1ac1237545
Evan Harris <echarris@smcm.edu>
2017-09-04 22:45:13 -0400

    Implements simple fizzbuzz solution

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,1 +1,13 @@
-print('hello world')
+index = 0
+
+while index != 16:
+    if index % 15 == 0:
+        print('Fizz buzz counting up to fizzbuzz')
+    elif index % 5 == 0:
+        print('Fizz buzz counting up to buzz')
+    elif index % 3 == 0:
+        print('Fizz buzz counting up to fizz')
+    else:
+        print('Fizz buzz counting up to {}'.format(index))
+    index += 1
+
